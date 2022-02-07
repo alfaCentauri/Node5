957901d7f0904608e77f0e994dcb25191b3c1314
Ricardo Presilla <rpresilla@initiumsoft.com>
2022-02-07 10:12:43 -0400

    Utilizando middelware

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const app = express();
 
-let html = '<div class="container-fluid"> <h1>Bienvenido al sitio web</h1> </div>';
-app.get("/home", (request, response) => {
+let isLogin = () => true;
+let logger = () => {
+    console.log("La petición es: %s", request.method );
+    next();
+};
+let showIp = () => {
+    console.log("La IP es: 127.0.0.1" );
+    next();
+};
+let html = '<div class="container-fluid"> <h1>Bienvenido al sitio web</h1> <br> <p>Sección publica.</p> </div>';
+app.use((request, response, next) => {
+    if(isLogin){
+        next();
+    }
+    else{
+        html += '<div class="container-fluid"> <p>No estas autorizado.</p> </div>';
+        response.send(html);
+    }
+}, logger, showIp);
+
+app.get("/", (request, response) => {
     response.send(html);
 });
+
+app.get("/home", (request, response) => {
+    html = '<div class="container-fluid"> <h1>Bienvenido al sitio web</h1> </div>';
+    response.send(html);
+});
+
 app.post("/login", (request, response) => {
     html += '<div class="container-fluid"><p class="login">Este es el login de usuario.</p></div>';
     response.send(html);
@@ -20,4 +45,6 @@ app.delete("/delete", (request, response) => {
     response.send(html);
 });
 
-app.listen(8085);
+app.listen(8085, () => {
+    console.log("Ejemplo de Rest Api, esperando en el puerto: 8085.");
+});
